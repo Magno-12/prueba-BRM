161d7530219b2538ad2aeaadea3e1a1d822e47a4
Magno Martinez <66977118+Magno-12@users.noreply.github.com>
2023-11-02 09:05:43 -0500

    fix(controllers): correct error messages

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,4 +1,4 @@
-const User = require('../models/User');
+const User = require('.../models/User');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const { SECRET_KEY } = require('../config');
@@ -8,12 +8,12 @@ exports.register = async (req, res) => {
     const userData = req.body;
 
     if (!userData.username || !userData.email || !userData.password) {
-      return res.status(400).json({ error: 'Todos los campos son obligatorios' });
+      return res.status(400).json({ error: 'All fields are required' });
     }
 
     const existingUser = await User.findOne({ where: { email: userData.email } });
     if (existingUser) {
-      return res.status(400).json({ error: 'El usuario ya está registrado' });
+      return res.status(400).json({ error: 'The user is already registered' });
     }
 
     const hashedPassword = await bcrypt.hash(userData.password, 10);
@@ -31,7 +31,7 @@ exports.register = async (req, res) => {
     return res.status(201).json({ token });
   } catch (error) {
     console.error(error);
-    return res.status(500).json({ error: 'Error al registrar el usuario' });
+    return res.status(500).json({ error: 'Error when registering the user' });
   }
 };
 
@@ -40,19 +40,19 @@ exports.login = async (req, res) => {
     const { email, password } = req.body;
 
     if (!email || !password) {
-      return res.status(400).json({ error: 'Correo electrónico y contraseña son obligatorios' });
+      return res.status(400).json({ error: 'Email and password are required' });
     }
 
     const user = await User.findOne({ where: { email } });
 
     if (!user) {
-      return res.status(401).json({ error: 'Credenciales inválidas' });
+      return res.status(401).json({ error: 'Invalid credentials' });
     }
 
     const passwordMatch = await bcrypt.compare(password, user.password);
 
     if (!passwordMatch) {
-      return res.status(401).json({ error: 'Credenciales inválidas' });
+      return res.status(401).json({ error: 'Invalid credentials' });
     }
 
     const token = jwt.sign({ userId: user.id }, SECRET_KEY, {
@@ -62,6 +62,6 @@ exports.login = async (req, res) => {
     return res.status(200).json({ token });
   } catch (error) {
     console.error(error);
-    return res.status(500).json({ error: 'Error al iniciar sesión' });
+    return res.status(500).json({ error: 'Error logging in' });
   }
 };
--- src/controllers/purchaseController.js
+++ src/controllers/purchaseController.js
@@ -1,20 +1,20 @@
-const Purchase = require('../models/purchase');
+const Purchase = require('.../models/purchase');
 
 exports.createPurchase = async (req, res) => {
         try {
           const purchaseData = req.body;
 
           if (!purchaseData.products || !Array.isArray(purchaseData.products) || purchaseData.products.length === 0) {
-            return res.status(400).json({ error: 'La compra debe incluir al menos un producto' });
+            return res.status(400).json({ error: 'The purchase must include at least one product' });
           }
 
           for (const productData of purchaseData.products) {
             const product = await Product.findByPk(productData.productId);
             if (!product) {
-              return res.status(400).json({ error: `Producto con ID ${productData.productId} no encontrado` });
+              return res.status(400).json({ error: `Product with ID ${productData.productId} not found` });
             }
             if (productData.quantity > product.quantityAvailable) {
-              return res.status(400).json({ error: `Cantidad insuficiente de ${product.name}` });
+              return res.status(400).json({ error: `Insufficient quantity of ${product.name}` });
             }
           }
           const purchaseInfo = {
@@ -38,9 +38,9 @@ exports.createPurchase = async (req, res) => {
             await product.save();
           }
 
-          return res.status(201).json({ message: 'Compra registrada con éxito' });
+          return res.status(201).json({ message: 'Purchase successfully registered' });
         } catch (error) {
           console.error(error);
-          return res.status(500).json({ error: 'Error al registrar la compra' });
+          return res.status(500).json({ error: 'Error registering purchase' });
         }
       };
