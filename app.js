7bc0a3e0dcc51259e72521f8e958c0da2a3ca821
Magno Martinez <66977118+Magno-12@users.noreply.github.com>
2023-11-02 18:50:17 -0500

    feat(app): implement log and error capture

--- app.js
+++ app.js
@@ -2,15 +2,21 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
+const errorHandler = require('./src/middlewares/errorHandling');
+
 const authRoutes = require('./src/routes/authRoutes');
 const productRoutes = require('./src/routes/productRoutes');
 const purchaseRoutes = require('./src/routes/purchaseRoutes');
 const userRoutes = require('./src/routes/userRoutes');
 
+app.use(express.json());
+
 app.use('/auth', authRoutes);
 app.use('/products', productRoutes);
 app.use('/purchases', purchaseRoutes);
 app.use('/users', userRoutes);
+
+app.use(errorHandler);
 
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
